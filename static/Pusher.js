069e2287261ee4c2d40748f5f2aa4232546b6bdd
Vlad Otrocol <vladotrocol@yahoo.com>
2013-06-23 07:09:59 +0100

    working pusher code

--- static/Pusher.js
+++ static/Pusher.js
@@ -1,13 +1,22 @@
 var pusher = new Pusher('25c5f81316b913b402ca');
-var channel = pusher.subscribe('private-presentation-channel');
-channel.bind('client-move-to-slide', function(data) {
-	console.log('Received message to move to slide ' + self.current_slide_number);
-    ev_cap.moveToSlide(data.current_slide_number);
-});
+var privateChannel = pusher.subscribe('private-channel');
+
+Pusher.log = function(message) {
+	if (window.console && window.console.log) {
+		window.console.log(message);
+	}
+};
 
 function pusherPush(number){
-	channel.trigger('private-presentation-channel', 'client-move-to-slide', {
-  		"current_slide_number": number
+	
+	privateChannel.bind('pusher:subscription_succeeded', function(data) { 
+		privateChannel.trigger('client-event', {"message": number});
+		ev_cap.moveToSlide(data.message);
 	});
-	console.log('moving to slide ' + number);
+
+	privateChannel.bind('pusher:subscription_error', function(status) {
+	  if(status == 408 || status == 503){
+	  	console.log("AUTH error: " + status);
+	  }
+	});
 };
